485cee05d3e8f4318a0043ab93218c53a5e4a34e
Mark Vartanyan <kolypto@gmail.com>
2014-08-04 15:16:51 +0200

    package

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Build Status](https://api.travis-ci.org/kolypto/py-exdoc.png?branch=master)](https://travis-ci.org/kolypto/py-exdoc)
+
+
+ExDoc
+=====
+
+Documentation extractor.
+
+Extracts pieces of documentation from your code to build a document which can be fed to template processors.
+
+Output can be in JSON, YAML, whatever.
+Use any command-line templating engine, like [j2cli](https://github.com/kolypto/j2cli), to render templates from it.
+
+It does not do any automatic background magic: it just offers helpers which allows you to extract the necessary pieces.
+
+
+
+
+Collector
+=========
+
+
+
+Extractors
+----------
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+""" Documentation extractor. """
+
+from setuptools import setup, find_packages
+
+setup(
+    name='exdoc',
+    version='0.0.1-0',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-exdoc',
+    license='BSD',
+    description=__doc__,
+    long_description=open('README.rst').read(),
+    keywords=['documentation'],
+
+    packages=find_packages(),
+    scripts=[],
+    entry_points={},
+
+    install_requires=[
+    ],
+    extras_require={
+        '_tests': ['nose', ],
+    },
+    include_package_data=True,
+    test_suite='nose.collector',
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
